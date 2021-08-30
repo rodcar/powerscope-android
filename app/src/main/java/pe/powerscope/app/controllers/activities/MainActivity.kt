b2032da50a93772f4d5a8695d7ff01cbfc46f2ad
rodcar <ivanrod12@hotmail.com>
2021-08-30 18:45:05 -0500

    Commit inicial

---
+++ app/src/main/java/pe/powerscope/app/controllers/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package pe.powerscope.app.controllers.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pe.powerscope.app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
